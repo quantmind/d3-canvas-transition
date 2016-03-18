4f0c7b3b0f72a1a24c611ee7d0a43fa0e63b420f
Luca Sbardella <luca@quantmind.com>
2016-03-18 12:05:01 +0000

    added axis

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ export {default as select} from './src/select';
 export {default as tweenAttr} from './src/tween';
 export {default as resolution} from './src/utils';
 export {CanvasElement} from './src/element';
+export {axisTop, axisRight, axisBottom, axisLeft} from './src/axis';
 export {version} from './package.json';
