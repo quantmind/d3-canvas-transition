c9b7177918590af5030f37f9fddc3cea4b2880d4
Luca Sbardella <luca@quantmind.com>
2016-12-25 10:03:51 +0000

    export getSize

--- test/utils-test.js
+++ test/utils-test.js
@@ -7,5 +7,10 @@ test('Test getSize', (t) => {
     t.equal(getSize(), undefined);
     var d = select(document.createElement('div')).style('width', '960px');
     t.equal(getSize(d.style('width')), 960);
+    t.equal(getSize(10), 10);
+    t.equal(getSize('10%', 1000), 100);
+    t.equal(getSize('10%'), 0.1);
+    t.equal(getSize('0.7em'), 0.7);
+    t.equal(getSize('0.7em', 10), 7);
     t.end();
 });
