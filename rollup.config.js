201ea2edea2792d3adc1d4ae835207320c59b55c
Luca Sbardella <luca@quantmind.com>
2017-07-18 22:23:24 +0100

    new minor version

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,29 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 
 export default {
     entry: 'index.js',
     format: 'umd',
     moduleName: 'd3',
-    moduleId: 'd3-canvas-transition',
     plugins: [
         json(),
         babel({
             babelrc: false,
             presets: ['es2015-rollup']
-        })
+        }),
+        sourcemaps()
     ],
+    extend: true,
+    sourceMap: true,
     dest: 'build/d3-canvas-transition.js',
+    external: [
+        'd3-collection',
+        'd3-color',
+        'd3-selection',
+        'd3-timer'
+    ],
     globals: {
         'd3-collection': 'd3',
         'd3-color': 'd3',
